a7d5885e4751982c092447842113e7c43e05c8a9
Andrey Laguta <cirkus.kz@gmail.com>
2020-04-20 20:31:49 +0300

    add filter and get to db service

--- aiohttp_rest_framework/db.py
+++ aiohttp_rest_framework/db.py
@@ -4,7 +4,7 @@ import typing
 
 from aiopg.sa import Engine
 from aiopg.sa.result import ResultProxy, RowProxy
-from sqlalchemy import Table
+from sqlalchemy import Table, and_
 
 from aiohttp_rest_framework import types
 
@@ -13,6 +13,14 @@ class DatabaseServiceABC(metaclass=abc.ABCMeta):
     def __init__(self, connection, model=None):
         self.connection = connection
         self.model = model
+
+    @abc.abstractmethod
+    async def get(self, where: typing.Mapping = None):
+        pass
+
+    @abc.abstractmethod
+    async def filter(self, where: typing.Mapping = None):
+        pass
 
     @abc.abstractmethod
     async def create(self, data: typing.Mapping):
@@ -31,6 +39,20 @@ class AioPGService(DatabaseServiceABC):
     connection: Engine
     model: Table
 
+    async def get(self, where: typing.Mapping = None) -> typing.Optional[RowProxy]:
+        if where:
+            where = and_(self.model.columns[key] == value for key, value in where.items())
+
+        query = self.model.select(where).limit(1)
+        return await self.execute(query, fetch="one")
+
+    async def filter(self, where: typing.Mapping = None) -> typing.List[RowProxy]:
+        if where:
+            where = and_(self.model.columns[key] == value for key, value in where.items())
+
+        query = self.model.select(where)
+        return await self.execute(query, fetch="all")
+
     async def create(self, data: typing.Mapping) -> RowProxy:
         query = self.model.insert().values(**data).returning(*self.model.columns)
         return await self.execute(query, fetch="one")
@@ -42,10 +64,10 @@ class AioPGService(DatabaseServiceABC):
             *,
             pk_field: str = "id",
     ) -> RowProxy:
-        where = {pk_field: pk}
-        query = self.model.update(**where).values(**data)
+        where = self.model.columns[pk_field] == pk
+        query = self.model.update(where).values(**data)
         await self.execute(query)
-        return await self.execute(self.model.select(**where), fetch="one")
+        return await self.execute(self.model.select(where), fetch="one")
 
     async def delete(self, pk: typing.Any, *, pk_field: str = "id") -> ResultProxy:
         query = self.model.delete(**{pk_field: pk})
@@ -60,7 +82,7 @@ class AioPGService(DatabaseServiceABC):
         async with self.connection.acquire() as conn:
             result: ResultProxy = await conn.execute(query)
             if fetch is not None:
-                if fetch == 'all':
+                if fetch == "all":
                     return await result.fetchall()
                 return await result.fetchone()
             return result
