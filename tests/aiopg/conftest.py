8ca931a4581f13eb0599afadb77732523042fba7
Andrey Laguta <cirkus.kz@gmail.com>
2020-04-21 15:01:52 +0300

    add user fixture

--- tests/aiopg/conftest.py
+++ tests/aiopg/conftest.py
@@ -1,1 +1,14 @@
+import pytest
+from aiopg.sa.result import ResultProxy
 
+from tests import models
+from tests.aiopg.utils import async_engine_connection
+
+
+@pytest.fixture
+async def user():
+    async with async_engine_connection() as engine:
+        async with engine.acquire() as conn:
+            query = models.users.select().limit(1)
+            result: ResultProxy = await conn.execute(query)
+            yield await result.fetchone()
